183281375953967f9f5f4fa14edf4ee153f5166c
Harshwardhan Patil <harshwardhanpatil340@gmail.com>
2024-10-31 18:24:12 +0530

    Working on nav Search

--- src/partials/Topnav.jsx
+++ src/partials/Topnav.jsx
@@ -14,7 +14,9 @@ const Topnav = () => {
           onChange={(e)=> setquery(e.target.value)}
           value={query}
            className="w-[50%] mx-10 p-5 text-xl outline-none border-none bg-transparent text-zinc-400  " type="text" placeholder="Search anything" />
-          <i class=" text-zinc-400 text-3xl ri-close-fill"></i>
+
+           {query.length > 0 && <i class=" text-zinc-400 text-3xl ri-close-fill"></i> }
+          
 
           <div className="absolute w-[50%] h-[50vh] bg-zinc-200 top-[90%] overflow-auto">
             <Link className=" hover:text-black hover:bg-zinc-300 duration-200  font-semibold text-zinc-600 w-[100%] p-10 flex justify-start items-center border-b-2 border-zinc-100  ">
