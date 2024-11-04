9e74ea14667e72605be68e0cef605cdb2fdd9a75
Harshwardhan Patil <harshwardhanpatil340@gmail.com>
2024-11-04 19:44:22 +0530

    Topnav Complted

--- src/partials/Sidenav.jsx
+++ src/partials/Sidenav.jsx
@@ -9,25 +9,25 @@ function Sidenav() {
   return (
     <div className="w-[20%] h-full border-r-2 border-zinc-200 p-10">
          <h1 className="text-2xl text-white font-bold mr-3">
-         <i class= " text-[#6556CD] ri-tv-fill"></i>
+         <i className= " text-[#6556CD] ri-tv-fill"></i>
           <span className="text-2xl">PrimeMax</span> 
          </h1>
 
          <nav className="flex flex-col text-zinc-400 text-xl gap-3">
          <h1 className="text-white font-semibold text-xl mt-8 mb-5">New Feeds</h1>
-         <Link  className='hover:bg-[#6556CD] hover:text-white duration-300 rounded-lg p-4 '  > <i class="mr-2 ri-fire-fill"></i> Tranding</Link>
-         <Link className='hover:bg-[#6556CD] hover:text-white duration-300 rounded-lg p-4 ' > <i class="mr-2 ri-bard-fill"></i> Popular</Link>
-         <Link className='hover:bg-[#6556CD] hover:text-white duration-300 rounded-lg p-4 ' > <i class="mr-2 ri-movie-2-ai-fill"></i> Movies</Link>
-         <Link className='hover:bg-[#6556CD] hover:text-white duration-300 rounded-lg p-4 ' > <i class="mr-2 ri-slideshow-3-fill"></i> Tv Shows</Link>
-         <Link className='hover:bg-[#6556CD] hover:text-white duration-300 rounded-lg p-4 ' ><i class="mr-2 ri-team-fill"></i> People</Link>
+         <Link  className='hover:bg-[#6556CD] hover:text-white duration-300 rounded-lg p-4 '  > <i className="mr-2 ri-fire-fill"></i> Tranding</Link>
+         <Link className='hover:bg-[#6556CD] hover:text-white duration-300 rounded-lg p-4 ' > <i className="mr-2 ri-bard-fill"></i> Popular</Link>
+         <Link className='hover:bg-[#6556CD] hover:text-white duration-300 rounded-lg p-4 ' > <i className="mr-2 ri-movie-2-ai-fill"></i> Movies</Link>
+         <Link className='hover:bg-[#6556CD] hover:text-white duration-300 rounded-lg p-4 ' > <i className="mr-2 ri-slideshow-3-fill"></i> Tv Shows</Link>
+         <Link className='hover:bg-[#6556CD] hover:text-white duration-300 rounded-lg p-4 ' ><i className="mr-2 ri-team-fill"></i> People</Link>
          </nav>
 
          <hr className="bg-none h-[1px] mt-[5px] bg-zinc-400" />
 
          <nav className="flex flex-col text-zinc-400 text-xl gap-3">
          <h1 className="text-white font-semibold text-xl mt-10 mb-5">Website Information</h1>
-         <Link  className='hover:bg-[#6556CD] hover:text-white duration-300 rounded-lg p-4 '  > <i class="mr-2 ri-information-fill"></i> About</Link>
-         <Link className='hover:bg-[#6556CD] hover:text-white duration-300 rounded-lg p-4 ' > <i class="mr-2 ri-phone-fill"></i> Contact</Link>
+         <Link  className='hover:bg-[#6556CD] hover:text-white duration-300 rounded-lg p-4 '  > <i className="mr-2 ri-information-fill"></i> About</Link>
+         <Link className='hover:bg-[#6556CD] hover:text-white duration-300 rounded-lg p-4 ' > <i className="mr-2 ri-phone-fill"></i> Contact</Link>
 
          </nav>
     </div>
--- src/partials/Topnav.jsx
+++ src/partials/Topnav.jsx
@@ -1,6 +1,7 @@
 import axios from "../utils/axios";
 import { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
+import noimage from "/noimage.jpg"
 
 
 const Topnav = () => {
@@ -27,20 +28,27 @@ useEffect(()=>{
 
   return (
     <div className="w-full h-[10vh] relative flex justify-start items-center ml-[15%]">
-          <i class=" text-zinc-400 text-3xl ri-search-line"></i>
+          <i className=" text-zinc-400 text-3xl ri-search-line"></i>
           <input
           onChange={(e)=> setquery(e.target.value)}
           value={query}
            className="w-[50%] mx-10 p-5 text-xl outline-none border-none bg-transparent text-zinc-400  " type="text" placeholder="Search anything" />
 
-           {query.length > 0 && <i onClick={()=>setquery("")} class=" text-zinc-400 text-3xl ri-close-fill"></i> }
+           {query.length > 0 && <i onClick={()=>setquery("")} className=" text-zinc-400 text-3xl ri-close-fill"></i> }
           
           
           <div className="absolute w-[50%] max-h-[50vh] bg-zinc-200 top-[90%] overflow-auto rounded"> 
              {searches.map((s,i)=><Link key={i} className=" hover:text-black hover:bg-zinc-300 duration-200  font-semibold text-zinc-600 w-[100%] p-10 flex justify-start items-center border-b-2 border-zinc-100  ">
             <img
-            className="w-[10vh] h-[10vh] object-cover rounded mr-5"
-             src={`https://image.tmdb.org/t/p/original/${s.backdrop_path}`} alt="" />
+            className="w-[10vh] h-[10vh] object-cover rounded mr-5 shadow-lg"
+
+            src={
+              s.backdrop_path ||
+              s.profile_path? `https://image.tmdb.org/t/p/original/${
+              s.backdrop_path || s.profile_path
+              }` : noimage
+
+             } alt="" />
             <span>{s.title || s.name || s.original_name || s.original_title}</span>
             </Link>)} 
               
